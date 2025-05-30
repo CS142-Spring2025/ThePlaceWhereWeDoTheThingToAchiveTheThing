90feb9444ff3c23f54093d9aa14fb2db12827fa3
WilliamKur <williammikasa8@gmail.com>
2025-05-29 18:47:36 -0700

    Update Grass.java

--- Grass.java
+++ Grass.java
@@ -1,13 +1,16 @@
+//these line show all the import used in the program
 import java.awt.Color;
 
-
+//this is where the Grass class started
 public class Grass extends Tiles{
     protected Color color;
 
+    //this constructor is used to create a Grass object
     public Grass() {
         super(Color.GREEN);
     }
 
+    //this method determine if the object is burnable
     public boolean burn(){
         return true;
     }
