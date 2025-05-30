fc50d9914efbf6e78bfd535c99e68fce53eecc80
WilliamKur <williammikasa8@gmail.com>
2025-05-29 19:58:08 -0700

    Create Sand.java

---
+++ Sand.java
@@ -0,0 +1,14 @@
+//these line show all the import used in the program
+package FinalProject;
+import java.awt.Color;
+
+//this is where the Rock class started
+public class Sand extends Tiles{
+    protected Color color;
+
+    //this constructor is used to create a Rock object
+    public Sand() {
+        super(new Color(255, 253, 208));
+    }
+    
+}
