5fa023fc56b722315f710a692b04b63da83ece47
OLDavies <Owendavies234@gmail.com>
2025-06-03 18:40:15 -0700

    Create BurnableTiles.java

---
+++ BurnableTiles.java
@@ -0,0 +1,51 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BurnableTiles extends Tiles {
+    private int remainingBurnTime;
+    private boolean isBurning;
+    private boolean flag = false;
+    public boolean canBurn;
+
+    public BurnableTiles(int burnTime, Color color) {
+        super(color);
+        this.canBurn = true;
+        this.remainingBurnTime = burnTime;
+        this.isBurning = false;
+    }
+
+    public int getRemainingBurnTime() {
+        return remainingBurnTime;
+    }
+
+    public void decreaseBurnTime(int timeStep, Tiles[][] grid, int row, int col) {
+        if (isBurning) {
+           remainingBurnTime -= timeStep;
+           if (remainingBurnTime <= 0) {
+               grid[row][col] = new Tiles();
+           }
+       }
+    }
+
+    public boolean isBurntOut() {
+        return this.remainingBurnTime <= 0;
+    }
+
+    public boolean isBurning() {
+        return isBurning;
+    }
+
+    public void setBurning(boolean burning) {
+        this.isBurning = burning;
+    }
+
+    @Override
+    public void draw(Graphics g, int x, int y, int tileSize) {
+        if (isBurning) {
+            g.setColor(Color.RED);
+            g.fillRect(x, y, tileSize, tileSize);
+        } else {
+            super.draw(g, x, y, tileSize);
+        }
+    }
+}
