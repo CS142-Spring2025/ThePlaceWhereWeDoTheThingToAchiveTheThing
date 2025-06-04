747eb4ae35904f466088b7543edebd6eff79f846
OLDavies <Owendavies234@gmail.com>
2025-06-03 18:39:19 -0700

    Update Tiles.java

--- Tiles.java
+++ Tiles.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 //this is where the Tiles class started
 public class Tiles {
     protected Color color;
+    protected boolean canBurn;
+    protected int burnTime;
 
     //this contructor is used to create Tiles object with default colour set
     public Tiles() {
@@ -23,6 +25,10 @@ public class Tiles {
 
     //this method is used to draw and set the tiles with sepcific colour and dimension
     public void draw(Graphics g, int x, int y, int size) {
+        g.setColor(color);
+        g.fillRect(x, y, size, size);
+    }
+    public void draw(Graphics g, int x, int y, int size, Color color) {
         g.setColor(color);
         g.fillRect(x, y, size, size);
     }
