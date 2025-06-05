58fc7b68fc2d5c590c6b9d7ac489ca405c87eacd
OLDavies <Owendavies234@gmail.com>
2025-06-05 12:51:36 -0700

    Create Ash.java

---
+++ Ash.java
@@ -0,0 +1,12 @@
+//these line show all the import used in the program
+import java.awt.Color;
+
+//this is where the Ash class started
+public class Ash extends Tiles{
+
+    //this constructor is used to create a Ash object
+    public Ash() {
+        super(new Color(55, 55, 52));
+    }
+    
+}
