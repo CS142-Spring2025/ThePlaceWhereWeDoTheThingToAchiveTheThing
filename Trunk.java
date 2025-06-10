91e76bbdd7330924497f73f6caf2b915c24e76ec
Sam <rocketbeau1@gmail.com>
2025-06-10 12:27:48 -0700

    Update Trunk.java
    twunk

--- Trunk.java
+++ Trunk.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 public class Trunk extends BurnableTiles{
 
    public Trunk() {
-        super(2000, new Color(105,75,55));
+        super(4000, 35, new Color(165,99,60));
    }
 
 
