c00d390a00b7d576cca8c4ab121939c7b7617a0e
Sam <rocketbeau1@gmail.com>
2025-06-10 12:28:59 -0700

    Update Interface.java
    sam has done things

--- Interface.java
+++ Interface.java
@@ -1,5 +1,5 @@
 //this line let the program to use resources from the FinalProject Package
-package FinalProject;
+//package FinalProject;
 
 //these line show the import used in the program
 import java.util.*;
@@ -15,12 +15,18 @@ public class Interface implements ActionListener {
     private JLabel grassLabel;
     private JLabel rockLabel;
     private JLabel fieldLabel;
+    private JLabel treeLabel;
+    private JLabel tickSpeedLabel;
     private JTextField grass;
     private JTextField rocks;
     private JTextField field;
-    private int grassVal = 30;
-    private int rocksVal = 25;
-    private int fieldVal = 45;
+    private JTextField tree;
+    private JTextField tickSpeed;
+    private int grassVal = 75;
+    private int rocksVal = 10;
+    private int fieldVal = 10;
+    private int treeVal=5;
+    private int tickSpeedVal = 200;
     private JFrame frame;
     private boolean status = false;
     
@@ -34,7 +40,9 @@ public class Interface implements ActionListener {
                 int newgrassVal = Integer.parseInt(grass.getText().trim());
                 int newrocksVal = Integer.parseInt(rocks.getText().trim());
                 int newfieldVal = Integer.parseInt(field.getText().trim());
-                int total = newgrassVal+newrocksVal+newfieldVal;
+                int newTreeVal = Integer.parseInt(tree.getText().trim());
+                int newTickSpeedVal = Integer.parseInt(tickSpeed.getText().trim());
+                int total = newgrassVal+newrocksVal+newfieldVal+newTreeVal;
                 if(total>100){
                     JOptionPane.showMessageDialog(frame, "The total value exceeds 100%.");
                 }else if(total<100){
@@ -43,6 +51,8 @@ public class Interface implements ActionListener {
                     grassVal = newgrassVal;
                     rocksVal = newrocksVal;
                     fieldVal = newfieldVal;
+                    treeVal = newTreeVal;
+                    tickSpeedVal = newTickSpeedVal;
                     status = true;
                     frame.dispose();
                 }
@@ -54,6 +64,8 @@ public class Interface implements ActionListener {
             grass.setText(Integer.toString(grassVal));
             rocks.setText(Integer.toString(rocksVal));
             field.setText(Integer.toString(fieldVal));
+            tree.setText(Integer.toString(treeVal));
+            tickSpeed.setText(Integer.toString(tickSpeedVal));
         }    
         
      
@@ -62,7 +74,7 @@ public class Interface implements ActionListener {
     //this constructor is used to create a Interface object
     public Interface(){
         frame = new JFrame("Interface");
-        frame.setSize(350,200);
+        frame.setSize(350,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         
@@ -90,14 +102,30 @@ public class Interface implements ActionListener {
         frame.add(fieldLabel);
         frame.add(field);
         
+        treeLabel = new JLabel("%  Tree Percentage");
+        treeLabel.setBounds(130,120,150,20);
+        tree = new JTextField(Integer.toString(treeVal));
+        tree.setBounds(50,120,80,20);
+        tree.setHorizontalAlignment(JTextField.RIGHT);
+        frame.add(treeLabel);
+        frame.add(tree);
+        
+        tickSpeedLabel = new JLabel(" Tick Speed (Miliseconds)");
+        tickSpeedLabel.setBounds(130,150,150,20);
+        tickSpeed = new JTextField(Integer.toString(tickSpeedVal));
+        tickSpeed.setBounds(50,150,80,20);
+        tickSpeed.setHorizontalAlignment(JTextField.RIGHT);
+        frame.add(tickSpeedLabel);
+        frame.add(tickSpeed);
+        
         submitButton = new JButton("Submit");
-        submitButton.setBounds(50,120,100,20);
+        submitButton.setBounds(50,180,100,20);
         submitButton.setHorizontalAlignment(JTextField.CENTER);
         submitButton.addActionListener(this);
         frame.add(submitButton);
         
         resetButton = new JButton("Reset");
-        resetButton.setBounds(170,120,100,20);
+        resetButton.setBounds(170,180,100,20);
         resetButton.setHorizontalAlignment(JTextField.CENTER);
         resetButton.addActionListener(this);
         frame.add(resetButton);
@@ -116,6 +144,14 @@ public class Interface implements ActionListener {
     //this method is used to return the percentage of field inputed by user
     public int FieldValue(){
         return fieldVal;
+    }
+    
+    public int treeValue(){
+      return treeVal;
+    }
+    
+    public int tickSpeedValue(){
+      return tickSpeedVal;
     }
     
     //this method is used to return the status of which the interface stage is completed
