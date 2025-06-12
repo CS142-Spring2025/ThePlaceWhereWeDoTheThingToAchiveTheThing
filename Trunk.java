911a05bda863fd30462e9b8d40665ab923c87148
WilliamKur <williammikasa8@gmail.com>
2025-06-11 22:04:10 -0700

    Update Trunk.java

--- Trunk.java
+++ Trunk.java
@@ -1,8 +1,10 @@
-
+//this is some of the import used in the program
 import java.awt.Color;
 
+//this is where the Trunk Class started
 public class Trunk extends BurnableTiles{
 
+   //this constructor is used to create the Trunk object
    public Trunk() {
         super(4000, 35, new Color(165,99,60));
    }
