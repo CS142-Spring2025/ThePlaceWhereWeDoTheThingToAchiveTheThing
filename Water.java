2c186608404b4ee5bd3dd1a8f58fb58783c34862
WilliamKur <williammikasa8@gmail.com>
2025-06-11 22:04:18 -0700

    Update Water.java

--- Water.java
+++ Water.java
@@ -1,8 +1,13 @@
+//these are some of the import used in the program
 import java.awt.Color;
 
-
+//this is where the water class started
 public class Water extends Tiles{
+    
+    //this is some of the variable used in the program
+    protected Color color;
 
+    //this constructor is used to create the water object
     public Water() {
         super(Color.BLUE);
     }
